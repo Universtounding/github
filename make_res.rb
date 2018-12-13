6f8a6e808ad9261c9408088eec896b2bb2e2b8b1
universtounding <kendmorse@gmail.com>
2018-12-13 18:32:46 -0500

    condensing code

--- make_res.rb
+++ make_res.rb
@@ -2,35 +2,14 @@
 
 require_relative 'reservation.rb'
 
-valid = Inherit.new
-res = Reserve.new
-
-res.person
-20.times { print "-" }
-puts
-valid.emailer
-20.times { print "-" }
-puts
-valid.is_valid_email?(@email)
-20.times { print "-" }
-puts
-res.calender
-20.times { print "-" }
-puts
-res.time_slot
-20.times { print "-" }
-puts
-res.party
-20.times { print "-" }
-puts
-puts "We will email you with any updates at #{valid.email}"
-
+create_user = NewRes.new
+p create_user
 
 res_hash = {
-  name: res.name, day: res.day, what_time: res.what_time, size: res.size, email: valid.email
+  name: create_user.name, day: create_user.day, what_time: create_user.what_time, size: create_user.size, email: create_user.email
 }
 
 
 reservations = File.new("reservation.txt", 'a+')
-reservations.puts("Name: #{res.name}", "Date: #{res.day}", "Time: #{res.what_time}", "People: #{res.size}", "Email: #{valid.email}", "Time_rec: #{Time.now}")
+reservations.puts("Name: #{create_user.name}", "Date: #{create_user.day}", "Time: #{create_user.what_time}", "People: #{create_user.size}", "Email: #{create_user.email}", "Time_rec: #{Time.now}")
 reservations.close
--- reservation.rb
+++ reservation.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 class Reserve
   attr_accessor :name, :day, :what_time, :size, :email
 
@@ -29,12 +31,12 @@ end
 
 def party
   puts "how many are you?"
-    size = gets.chomp.to_i
-      @size = size
-        if size > 10
-          puts "that is too many people"
-        else
-          puts "Just to confirm your reservation is for #{@name}, on #{@day}, at #{@what_time}, with #{@size}?"
+  size = gets.chomp.to_i
+  @size = size
+    if size > 10
+      puts "that is too many people"
+    else
+      puts "Just to confirm your reservation is for #{@name}, on #{@day}, at #{@what_time}, with #{@size}?"
     end
   end
 end
@@ -45,8 +47,8 @@ VALID = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
 def emailer
   puts "What is your email?"
-    email = gets.chomp
-      @email = email
+  email = gets.chomp
+    @email = email
 end
 
 def is_valid_email? email
@@ -54,17 +56,45 @@ def is_valid_email? email
     puts "That's a nice email!"
   else
     puts "#{email_redo}"
-     end
-   end
+  end
+end
 
 def email_redo
   puts "Please enter valid email!"
   email = gets.chomp
-    @email = email
+  @email = email
     if @email =~ VALID
       puts "That's a nice email!"
     else
       puts "#{email_redo}"
-       end
     end
   end
+end
+
+class NewRes < Inherit
+
+  def initialize
+    valid = Inherit.new
+    res = Reserve.new
+
+    res.person
+    20.times { print "-" }
+    puts
+    valid.emailer
+    20.times { print "-" }
+    puts
+    valid.is_valid_email?(@email)
+    20.times { print "-" }
+    puts
+    res.calender
+    20.times { print "-" }
+    puts
+    res.time_slot
+    20.times { print "-" }
+    puts
+    res.party
+    20.times { print "-" }
+    puts
+    puts "We will email you with any updates at #{valid.email}"
+  end
+end
