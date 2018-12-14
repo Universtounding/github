eadc46e8967ee971785d22c2df8115a862451301
universtounding <kendmorse@gmail.com>
2018-12-13 22:37:10 -0500

    still messing around with it

--- make_res.rb
+++ make_res.rb
@@ -2,14 +2,12 @@
 
 require_relative 'reservation.rb'
 
-create_user = NewRes.new
-p create_user
+res_hash = { name: storage.name, day: storage.day, what_time: storage.what_time, size: storage.size, email: storage.email }
+storage = Reserve.new
+party = NewRes.new
 
-res_hash = {
-  name: create_user.name, day: create_user.day, what_time: create_user.what_time, size: create_user.size, email: create_user.email
-}
-
+party.input
 
 reservations = File.new("reservation.txt", 'a+')
-reservations.puts("Name: #{create_user.name}", "Date: #{create_user.day}", "Time: #{create_user.what_time}", "People: #{create_user.size}", "Email: #{create_user.email}", "Time_rec: #{Time.now}")
+reservations.puts("Name: #{storage.name}", "Date: #{storage.day}", "Time: #{storage.what_time}", "People: #{storage.size}", "Email: #{storage.email}", "Time_rec: #{Time.now}")
 reservations.close
--- reservation.rb
+++ reservation.rb
@@ -3,41 +3,45 @@
 class Reserve
   attr_accessor :name, :day, :what_time, :size, :email
 
-def initialize
-      @name = name
-      @day = day
-      @what_time = what_time
-      @size = size
-      @email = email
+  def initialize
+    @name = name
+    @day = day
+    @what_time = what_time
+    @size = size
+    @email = email
   end
 
-def person
-  puts "who is making the reservation?"
-  name = gets.chomp.capitalize
-  @name = name
-end
+  def person
+    puts "who is making the reservation?"
+    name = gets.chomp.capitalize
+    @name = name
+  end
 
-def  calender
-  puts "what date is the reservation for?"
-  day = gets.chomp
-  @day = day
-end
+  def  calender
+    puts "what date is the reservation for?"
+    day = gets.chomp
+    @day = day
+  end
 
-def time_slot
-  puts "what time would you like to make the reservation?"
-  what_time = gets.chomp
-  @what_time = what_time
-end
+  def time_slot
+    puts "what time would you like to make the reservation?"
+    what_time = gets.chomp
+    @what_time = what_time
+  end
 
-def party
-  puts "how many are you?"
-  size = gets.chomp.to_i
-  @size = size
+  def party
+    puts "how many are you?"
+    size = gets.chomp.to_i
+    @size = size
     if size > 10
       puts "that is too many people"
     else
       puts "Just to confirm your reservation is for #{@name}, on #{@day}, at #{@what_time}, with #{@size}?"
     end
+  end
+
+  def final
+    res_hash = { name: @name, day: @day, what_time: @what_time, size: @size, email: @email }
   end
 end
 
@@ -48,7 +52,7 @@ VALID = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 def emailer
   puts "What is your email?"
   email = gets.chomp
-    @email = email
+  @email = email
 end
 
 def is_valid_email? email
@@ -73,28 +77,15 @@ end
 
 class NewRes < Inherit
 
-  def initialize
+  def input
     valid = Inherit.new
     res = Reserve.new
-
     res.person
-    20.times { print "-" }
-    puts
     valid.emailer
-    20.times { print "-" }
-    puts
     valid.is_valid_email?(@email)
-    20.times { print "-" }
-    puts
     res.calender
-    20.times { print "-" }
-    puts
     res.time_slot
-    20.times { print "-" }
-    puts
     res.party
-    20.times { print "-" }
-    puts
     puts "We will email you with any updates at #{valid.email}"
   end
 end
